e4ea3ce9bceff95491051b96754d7479b0c99c41
Keita Takahashi <takanenokei@gmail.com>
2026-03-05 22:25:12 +0900

    feat: add prime_numbers.php

---
+++ exercises/prime_numbers.php
@@ -0,0 +1,16 @@
+<?php
+	define("MAX_COUNT", 100);
+
+    for ($i = 2; $i < MAX_COUNT; $i++) {
+        $isPrime = true;
+
+        for ($j = 2; $j * $j <= $i; $j++) { 
+            if ($i % $j == 0) {
+                $isPrime = false;
+                break;
+            }
+        }
+        if ($isPrime) {
+            echo $i . '<br>';
+        }
+    }
