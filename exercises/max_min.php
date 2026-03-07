e1cdc65c149787ec220dd64617289585a888656e
Keita Takahashi <takanenokei@gmail.com>
2026-03-07 23:46:23 +0900

    feat: add max_min.php

---
+++ exercises/max_min.php
@@ -0,0 +1,25 @@
+<?php
+    const ARRAY_SIZE = 5;
+
+    $numbers = [];
+    for ($i = 0; $i < ARRAY_SIZE; $i++) {
+        $numbers[] = rand(1, 100);
+        echo $numbers[$i] . ' ';
+    }
+    echo '<br>';
+
+    $max = $numbers[0];
+    $min = $numbers[0];
+
+    for ($i = 0; $i < ARRAY_SIZE; $i++) {
+        if ($max < $numbers[$i]) {
+            $max = $numbers[$i];
+        }
+
+        if ($min > $numbers[$i]) {
+            $min = $numbers[$i];
+        }
+    }
+
+    echo 'max = ' . $max . '<br>';
+    echo 'min = ' . $min . '<br>';
