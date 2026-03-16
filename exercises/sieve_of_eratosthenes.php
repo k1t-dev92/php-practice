90444b2bae57716d77f86be981b7cd2f818a5950
Keita Takahashi <takanenokei@gmail.com>
2026-03-16 23:27:15 +0900

    feat: add sieve_of_eratosthenes.php

---
+++ exercises/sieve_of_eratosthenes.php
@@ -0,0 +1,27 @@
+<?php
+	const MAX_NUMBER = 100;
+
+	$isPrime = [];
+	for ($i = 0; $i <= MAX_NUMBER; $i++) {
+		$isPrime[$i] = false;
+	}
+
+	for ($i = 2; $i <= MAX_NUMBER; $i++) {
+		$isPrime[$i] = true;
+	}
+
+	for ($p = 2; $p * $p <= MAX_NUMBER; $p++) {
+		if (!$isPrime[$p]) {
+			continue;
+		}
+		for ($m = $p * $p; $m <= MAX_NUMBER; $m += $p) {
+			$isPrime[$m] = false;
+		}
+	}
+
+	for ($i = 2; $i <= MAX_NUMBER; $i++) {
+		if ($isPrime[$i]) {
+			echo $i . ' ';
+		}
+	}
+	echo '<br>';
