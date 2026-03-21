63cbe0732a519fdb16f8038d43dab95b2a2467d4
Keita Takahashi <takanenokei@gmail.com>
2026-03-21 18:50:23 +0900

    fix: use <= operator in prime_numbers loop to include MAX_COUNT

--- exercises/prime_numbers.php
+++ exercises/prime_numbers.php
@@ -1,7 +1,7 @@
 <?php
     const MAX_COUNT = 100;
 
-    for ($i = 2; $i < MAX_COUNT; $i++) {
+    for ($i = 2; $i <= MAX_COUNT; $i++) {
         $isPrime = true;
 
         for ($j = 2; $j * $j <= $i; $j++) { 
