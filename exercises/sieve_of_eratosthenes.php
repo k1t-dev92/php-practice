bf43800bdf29834b5250e3148095072f145783b5
Keita Takahashi <takanenokei@gmail.com>
2026-03-22 17:39:55 +0900

    style: fix indentation to use spaces consistently

--- exercises/sieve_of_eratosthenes.php
+++ exercises/sieve_of_eratosthenes.php
@@ -1,23 +1,23 @@
 <?php
-	const MAX_NUMBER = 100;
+    const MAX_NUMBER = 100;
 
-	$isPrime = [];
-	for ($i = 2; $i <= MAX_NUMBER; $i++) {
-		$isPrime[$i] = true;
-	}
+    $isPrime = [];
+    for ($i = 2; $i <= MAX_NUMBER; $i++) {
+        $isPrime[$i] = true;
+    }
 
-	for ($p = 2; $p * $p <= MAX_NUMBER; $p++) {
-		if (!$isPrime[$p]) {
-			continue;
-		}
-		for ($m = $p * $p; $m <= MAX_NUMBER; $m += $p) {
-			$isPrime[$m] = false;
-		}
-	}
+    for ($p = 2; $p * $p <= MAX_NUMBER; $p++) {
+        if (!$isPrime[$p]) {
+            continue;
+        }
+        for ($m = $p * $p; $m <= MAX_NUMBER; $m += $p) {
+            $isPrime[$m] = false;
+        }
+    }
 
-	for ($i = 2; $i <= MAX_NUMBER; $i++) {
-		if ($isPrime[$i]) {
-			echo $i . ' ';
-		}
-	}
-	echo '<br>';
+    for ($i = 2; $i <= MAX_NUMBER; $i++) {
+        if ($isPrime[$i]) {
+            echo $i . ' ';
+        }
+    }
+    echo '<br>';
